a39be2568607df48a76c04c18a4dbd9416564121
zcchen <zcchen@thoughtworks.com>
2020-09-30 16:49:10 +0800

    Add methods to bigquery client.

--- bigquery_client.py
+++ bigquery_client.py
@@ -1,3 +1,5 @@
+import io
+import json
 import os
 
 from google.cloud import bigquery
@@ -12,9 +14,8 @@ class BigqueryClient:
     def __init__(self, env='uat'):
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = f'client_utils/{Config.get_config(env, "GOOGLE_APPLICATION_CREDENTIALS")}'
 
-    def load_csv_file_into_bigquery(self, dataset_id, table_id, file_name, schema):
+    def load_csv_file_into_bigquery(self, dataset_id, table_id, csv_file_name, schema):
         client = bigquery.Client()
-
         dataset_ref = client.dataset(dataset_id)
         table_ref = dataset_ref.table(table_id)
         job_config = bigquery.LoadJobConfig()
@@ -22,17 +23,40 @@ class BigqueryClient:
         job_config.skip_leading_rows = 0
         job_config.write_disposition = 'WRITE_TRUNCATE'
         job_config.autodetect = True
+        job_config.allowQuotedNewlines = True
+        job_config.quote = '"'
         job_config.schema = schema
 
-        with open(file_name, "rb") as source_file:
+        with open(csv_file_name, "rb") as source_file:
             job = client.load_table_from_file(
                 source_file,
                 table_ref,
                 location="US",
                 job_config=job_config,
             )
-        job.result()
-        Logger.debug(f"op=load-csv-file-into-bigquery | status=OK | desc=Loaded {job.output_rows} rows into {dataset_id}:{table_id}")
+        try:
+            job.result()
+        except:
+            raise RuntimeError(f"op=load_csv_file_into_bigquery | status=Fail| desc=dataset_id: {dataset_id}, table_id: {table_id}, errors: {str(job.errors)}")
+        Logger.info(
+            f"op=load-csv-file-into-bigquery | status=OK | desc=Loaded {job.output_rows} rows into {dataset_id}:{table_id}")
+
+    def save_json_to_bigquery(self, dataset_id, table_id, schema, data, overwrite=False):
+        Logger.info("\nStart write to bigquery\n")
+        content = io.StringIO('\n'.join(json.dumps(record) for record in data))
+        client = bigquery.Client()
+        dataset_ref = client.dataset(dataset_id)
+        table_ref = dataset_ref.table(table_id)
+        job_config = bigquery.LoadJobConfig()
+        job_config.source_format = bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
+        if overwrite:
+            job_config.write_disposition = 'WRITE_TRUNCATE'
+        else:
+            job_config.write_disposition = 'WRITE_APPEND'
+        job_config.autodetect = True
+        job_config.schema = schema
+        client.load_table_from_file(content, table_ref, job_config=job_config).result()
+        Logger.info("\nFinish write to bigquery\n")
 
     def query_rows_from_bigquery(self, query_string):
         client = bigquery.Client()
@@ -78,3 +102,11 @@ class BigqueryClient:
         table_ref = self.create_connection(client, dataset_id, table_id)
         table = bigquery.Table(table_ref)
         client.delete_table(table, not_found_ok=True)
+
+    def read_view_of_dataset(self, dataset_id):
+        bq_client = bigquery.Client()
+        tables = list(bq_client.list_tables(dataset_id))
+        for table in tables:
+            view = bq_client.get_table(table)
+            with open(f'{view.table_id}.sql', 'w') as f:
+                f.write(view.view_query)
